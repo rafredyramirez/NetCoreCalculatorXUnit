caea1cf43d6611718ac1aa0bdfaa458b11e54cd2
rafredyramirez <ra.fredyramirez@gmail.com>
2024-11-22 09:14:52 -0500

    se agrega prueba unitaria a método adición

--- NetCoreCalculatorXUnit.Test/Services/Addition/AdditionServiceTest.cs
+++ NetCoreCalculatorXUnit.Test/Services/Addition/AdditionServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetCoreCalculatorXUnit.Services.Addition;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace NetCoreCalculatorXUnit.Test.Services.Addition
 {
     public class AdditionServiceTest
     {
+        [Fact]
+        public void Calculate_Addition() 
+        {
+            // Arrange
+            var a = 10;
+            var b = 15;
+            var expected = a + b;
+
+            var service = new AdditionService();
+
+            // Act
+            var result = service.Calculate(a, b);
+
+            // Asser
+            Assert.Equal(expected, result); 
+        }
+
+        [Theory]
+        [InlineData(5, 15, 20)]
+        [InlineData(25, 1, 26)]
+        [InlineData(40, 45, 85)]
+        [InlineData(5, 10, 15)]
+        public void Calculate_Addition_ResultEqualsExpected(int a, int b, int expected) 
+        { 
+            var additionService = new AdditionService();
+
+            var result = additionService.Calculate(a, b);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
